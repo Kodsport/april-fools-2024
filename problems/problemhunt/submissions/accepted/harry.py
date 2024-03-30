f472d7c3d73aa82cd8d6e2deea2d851fa2b1521a
matistjati <matistjati@outlook.com>
2024-03-30 03:35:09 +0100

    [problemhunt] sample

--- problems/problemhunt/submissions/accepted/harry.py
+++ problems/problemhunt/submissions/accepted/harry.py
@@ -7,5 +7,7 @@ if n.isnumeric():
     rows = [*map(int,input().split())]
 
     print(min(rows)+1)
+elif n=="Don't mind me":
+    print("Nothing to see here")
 else:
     print("APRIL{wait...why_1s_ther3_4_flag_h3re}")
